ab3994d72223fd3ac5a94fb4faeef227e5b99b6c
vatey <ritheavateychea@gmail.com>
2025-02-21 10:24:31 +0200

    Update

---
+++ demo/demo/src/main/java/com/example/demo/service/PersonService.java
@@ -0,0 +1,27 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dao.PersonDao;
+import com.example.demo.model.Person;
+
+@Service
+public class PersonService {
+    private final PersonDao personDao;
+
+    @Autowired
+    public PersonService(@Qualifier("fakedao") PersonDao persondao) {
+        this.personDao = persondao;
+    }
+    public int addPerson(Person person) {
+        return personDao.insertPerson(null, person);
+    }
+
+    public List<Person> getAllPeople() {
+        return personDao.selectAllPeople();
+    }
+}
